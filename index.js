3b68cebdfe0c3ea2fc375c05ee91205b478ad91b
Mahfuj Ahsan <mahfujur.rahman1@outlook.com>
2023-09-07 00:12:51 +0600

    add comment and explanation

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-// const jwt = require('jsonwebtoken');
 const connectDB = require('./utils/dbConnect');
-const menuRoutes = require("./routes/v1/menu.route.js");
-const bookingRoutes = require("./routes/v1/booking.route.js");
+const jwtRoutes = require("./routes/v1/jwt.route");
 const usersRoutes = require("./routes/v1/users.route.js");
+const menuRoutes = require("./routes/v1/menu.route.js");
+const cartRoutes = require("./routes/v1/carts.route");
+const bookingRoutes = require("./routes/v1/booking.route.js");
 const paymentIntendedRoutes = require("./routes/v1/paymentIntend.route.js");
 const paymentRoutes = require("./routes/v1/payments.route.js");
 const adminStatsRoutes = require("./routes/v1/adminStats.route.js");
 const orderStatsRoutes = require("./routes/v1/orderStats.route.js");
-const cartRoutes = require("./routes/v1/carts.route");
 const reviewRoutes = require("./routes/v1/reviews.route");
-const jwtRoutes = require("./routes/v1/jwt.route");
 const port = process.env.PORT || 5000;
 
 
@@ -22,26 +21,23 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/v1/booking', bookingRoutes)
+// api routes
+app.use('/api/v1/jwt', jwtRoutes)
 app.use('/api/v1/users', usersRoutes)
 app.use('/api/v1/users/admin', usersRoutes)
 app.use('/api/v1/menu', menuRoutes)
+app.use('/api/v1/carts', cartRoutes)
+app.use('/api/v1/reviews', reviewRoutes)
+app.use('/api/v1/booking', bookingRoutes)
 app.use('/api/v1/create-payment-intent', paymentIntendedRoutes)
 app.use('/api/v1/payments', paymentRoutes)
 app.use('/api/v1/admin-stats', adminStatsRoutes)
 app.use('/api/v1/order-stats', orderStatsRoutes)
-app.use('/api/v1/carts', cartRoutes)
-app.use('/api/v1/reviews', reviewRoutes)
-app.use('/api/v1/jwt', jwtRoutes)
 
 
 app.get('/', (req, res) => {
-    res.send('Bistro is running')
+    res.send('Server Running...')
 })
-
-// app.listen(port, () => {
-//     console.log(`Bistro is listening on port ${port}`)
-// });
 
 const database = 'bistroDb';
 
